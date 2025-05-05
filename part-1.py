0e7e4739d790bbeb09af1ff8428f9f9935e58366
Mikaela Baluyot <mikaebal@gmail.com>
2025-05-05 00:15:48 -0700

    part 1 and 2 submission completed

--- part-1.py
+++ part-1.py
@@ -4,17 +4,36 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError("negative numbers don't have factorials!")
+    if n == 0:
+        return 1
 
+    return n * factorial(n - 1)
 
 
 # reverse
+def reverse(text):
+    if text == "":
+        return ""
 
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
+def bunny(count):
+    if count == 0:
+        return 0
 
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
-
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    elif parens[0] == "(" and parens[-1] == ")":
+        return is_nested_parens(parens[1:-1])
+    else:
+        return False
--- part-2.py
+++ part-2.py
@@ -4,13 +4,36 @@
 # the appropriate comment.
 
 # search
+def search(array, query):
+    if not array:
+        return False
+    elif array[0] == query:
+        return True
 
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if text == "":
+        return True
+    elif text[0] == text[-1]:
+        return is_palindrome(text[1:-1])
+    else:
+        return False
 
 
 # digit_match
+def digit_match(digit_1, digit_2):
+    if digit_1 == 0 and digit_2 == 0:
+        return 1
+    
+    if digit_1 == 0 or digit_2 == 0:
+        return 0
+    
+    count = 1 if (digit_1 % 10 == digit_2 % 10) else 0
 
+    if digit_1 < 10 or digit_2 < 10:
+        return count
 
+    return count + digit_match(digit_1 // 10, digit_2 // 10)
